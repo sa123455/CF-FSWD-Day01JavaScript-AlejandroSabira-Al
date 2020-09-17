f87f2d7ea7ab88d22fdb7e43cc559e6cce6287db
Alejandro <alejandro.rodriguez95@gmail.com>
2020-09-17 12:36:15 +0200

    first try challenge

---
+++ challenge/script.js
@@ -0,0 +1,7 @@
+array = [["Vienna", "miau", "      ", "         ", "           ", "              "], ["is"], ["a"], ["nice"], ["city"]];
+
+console.log(array[0][0]);
+console.log(array[0][2], array[1][0]);
+console.log(array[0][3], array[2][0]);
+console.log(array[0][4], array[3][0]);
+console.log(array[0][5], array[4][0]);
