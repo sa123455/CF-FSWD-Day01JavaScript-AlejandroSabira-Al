2d055bd77a1defa5cc59e7bcae7ae9a1a76263a6
Sabira Aliu <sabiraaliu@gmail.com>
2020-09-17 10:11:10 +0200

    basicJavaScript

---
+++ javaScriptBasis/script.js
@@ -0,0 +1,6 @@
+console.log("Hi ma name is Martin");
+
+var age = 25;
+console.log(" I am " + age + " old ");
+var players = ["Marthin", "Thomas", "Peter", "Mathias", "Niki"];
+console.log("The most valuable player of the match is " + players[2]);
